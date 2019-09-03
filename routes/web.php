fc134a65eaf7339425be2dcf7421df5eab81eddc
Manuel Moreno <manuel.moreno@televisatim.com>
2019-09-03 18:48:29 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::group(['middleware' => ['guest']],function (){
+    Route::get('/','Auth\LoginController@showLoginForm');
+    Route::post('login', 'Auth\LoginController@login')->name('login');
+});
+
+Route::group(['middleware' => ['auth']],function (){
+    Route::get('/main', function () {
+        return view('contenido/contenido');
+    })->name('main');
+});
